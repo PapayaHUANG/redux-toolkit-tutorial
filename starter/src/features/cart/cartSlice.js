65029bb4906c748327217d9ccab7873ec54500c1
PapayaHUANG <hsy922@qq.com>
2022-04-23 17:42:51 +0800

    use axios and thunkAPI

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import cartItems from '../../cartItems';
+import axios from 'axios';
 
 const url = 'https://course-api.com/react-useReducer-cart-project';
 
@@ -10,11 +10,17 @@ const initialState = {
   isLoading: true,
 };
 
-export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
-  return fetch(url)
-    .then((res) => res.json())
-    .catch((err) => console.log(err));
-});
+export const getCartItems = createAsyncThunk(
+  'cart/getCartItems',
+  async (name, thunkAPI) => {
+    try {
+      const res = await axios(url);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('Something went wrong!');
+    }
+  }
+);
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -54,7 +60,8 @@ const cartSlice = createSlice({
       state.isLoading = false;
       state.cartItems = action.payload;
     },
-    [getCartItems.rejected]: (state) => {
+    [getCartItems.rejected]: (state, action) => {
+      console.log(action);
       state.isLoading = false;
     },
   },
